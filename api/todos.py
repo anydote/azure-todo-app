5b4ad452891242a53224ef0110c5b4f3b662d909
Antti <antti.maatta@kone.com>
2025-10-05 10:30:20 +0300

    Add Todo data model

--- api/todos.py
+++ api/todos.py
@@ -1,5 +1,24 @@
+from pydantic import BaseModel, Field, constr
+import uuid
+from datetime import datetime
 import azure.functions as func
-import logging
+
+
+class TodoItem(BaseModel):
+    PartitionKey: str = Field(default="todos", const=True)
+    RowKey: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    title: constr(max_length=200)
+    createdAt: str = Field(default_factory=lambda: datetime.utcnow().isoformat())
+
+    @property
+    def id(self) -> str:
+        return self.RowKey
+
+    def to_dict(self):
+        d = self.dict()
+        d["id"] = self.id
+        return d
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
